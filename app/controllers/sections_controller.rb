f6a3254f282714d8e1a782172154972d93a751c0
Diana Liao <dijali@gmail.com>
2021-04-20 14:49:47 -0400

    Add section show method

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,9 @@
 class SectionsController < ApplicationController
+
+  def show
+    section = Section.find(params[:id])
+
+    render json: section
+  end
+
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :name, :image, :quantity, :date_added, :expiration_date, :section_id
+  attributes :name, :image, :quantity, :date_added, :expiration_date, :section_id, :id
 
   belongs_to :section
 end 
