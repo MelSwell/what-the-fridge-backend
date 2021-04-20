d15057e69455ce70c10aa745aeaba83fbc9818e4
Seth M Cohen <smcohen82@gmail.com>
2021-04-20 12:52:45 -0400

    Adde create item action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,16 @@
 class ItemsController < ApplicationController
+  def create
+    item = Item.create(item_params)
+    if item.valid?
+      render json: item
+    else
+      render json: item.errors.full_messages
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :image, :quantity, :date_added, :expiration_date, :section_id)
+  end
 end
