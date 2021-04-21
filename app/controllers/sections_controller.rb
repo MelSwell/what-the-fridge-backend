18067303df214b889ef29e0a0ed00698e858245f
Seth M Cohen <smcohen82@gmail.com>
2021-04-20 22:38:13 -0400

    Add create section action

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -6,4 +6,13 @@ class SectionsController < ApplicationController
     render json: section
   end
 
+  def create
+    section = Section.create(params.permit(:name, :fridge_id))
+    if section.valid?
+      render json: section
+    else
+      render json: section.errors.full_messages
+    end
+  end
+
 end
