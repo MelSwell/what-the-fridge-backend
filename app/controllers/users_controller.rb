5fa6b247f2336132f66dc5bc976854ea7c0005a9
Seth M Cohen <smcohen82@gmail.com>
2021-04-22 19:01:52 -0400

    changed default trash section name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     fridge = Fridge.create(name: "#{user.name}'s Fridge", user_id: user.id)
-    Section.create(name: "Trash", fridge_id:fridge.id)
+    Section.create(name: "Wall of Shame", fridge_id:fridge.id)
     if user.valid?
       render json: user
     else
